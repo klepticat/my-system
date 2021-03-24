81894c09d65c35a0ee730c4143aea6e6213d951d
klepticat <ellie.a.private@gmail.com>
2021-03-24 09:48:35 -0400

    initial commit

---
+++ assets/js/api.js
@@ -0,0 +1,39 @@
+const http = new XMLHttpRequest();
+const api = 'https://api.pluralkit.me/v1';
+
+//gets system from discord id
+async function getSystem (id) {
+    let url = `${api}/a/${id}/`
+    result = await newRequest(url)
+    return JSON.parse(result)
+}
+
+//gets members from system id
+async function getMembers (id) {
+    let url = `${api}/s/${id}/members`
+    result = await newRequest(url)
+    return JSON.parse(result)
+}
+
+//creates a new request
+function newRequest (url) {
+
+    //send request
+    http.open('GET', url)
+    http.send()
+
+    //create httpState promise that resolves on completion
+    let httpState = new Promise((resolved, rejected) => {
+        http.onreadystatechange = (e) => {
+            if(http.readyState >= 4 && http.status==200) {
+                resolved(http.response);
+            }
+        }
+    })
+    return httpState.then((response) => {return response}, (error) => {logError(error)})
+}
+
+//log any errors
+function logError (error) {
+    console.log(`API Wrapper Error: ${error}`)
+}
---
+++ assets/js/search.js
@@ -0,0 +1,48 @@
+//search (called when button pressed)
+async function search () {
+    const id = document.getElementById('idSearch').value;
+    const sysInfo = document.getElementById('sysInfo')
+
+    //check if input is only numbers
+    if(id.match(/^\d+$/) != null) {
+        
+        //get system
+        getSystem(id).then(result => {
+            sysInfo.hidden = false;
+
+            //set system information
+            if(result.name != null) addInfo('sysName', result.name)
+            else clearInfo('sysName')
+            if(result.avatar_url != null) addInfo('sysIcon', result.avatar_url)
+            else clearInfo('sysIcon')
+            if(result.description != null) addInfo('sysDesc', result.description)
+            else clearInfo('sysDesc')
+            if(result.created != null) addInfo('sysCreated', result.created.substring(0,10))
+            else clearInfo('sysDesc')
+
+            //get members
+            getMembers(result.id).then(result => {
+
+            })
+        })
+        document.getElementById('idSearch').value = ''
+    } else {
+        console.log('Search returned an error.')
+    }
+}
+
+//add info and show element
+function addInfo (id, data) {
+    let element = document.getElementById(id)
+    if(element.hidden == true) element.hidden = false;
+    if(element.parentElement.hidden == true) element.parentElement.hidden = false;
+    element.innerHTML = data
+}
+
+//clear info and hide element
+function clearInfo (id) {
+    let element = document.getElementById(id)
+    if(element.hidden == false) element.hidden = true;
+    if(element.parentElement.hidden == false) element.parentElement.hidden = true;
+    element.innerHTML = ''
+}
